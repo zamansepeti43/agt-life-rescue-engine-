f5c4fc774436317260548c8a8550870e10a7aa0b
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-21 13:50:31 +0300

    feat: embed Android local notification engine

---
+++ android/app/src/main/java/com/agtstudio/liferescue/BootReceiver.java
@@ -0,0 +1,11 @@
+package com.agtstudio.liferescue;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) NotificationScheduler.restoreAll(context);
+    }
+}
