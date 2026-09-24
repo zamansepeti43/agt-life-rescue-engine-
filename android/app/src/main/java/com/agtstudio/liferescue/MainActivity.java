cb8b878d09f9d74c49b8f70ab06dbff4639245da
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-24 09:33:08 +0300

    Make AGT LIFE Android app immersive fullscreen

--- android/app/src/main/java/com/agtstudio/liferescue/MainActivity.java
+++ android/app/src/main/java/com/agtstudio/liferescue/MainActivity.java
@@ -5,6 +5,10 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
@@ -22,6 +26,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        enableImmersiveFullscreen();
 
         WebView webView = new WebView(this);
         WebSettings settings = webView.getSettings();
@@ -57,8 +62,35 @@ public class MainActivity extends Activity {
         webView.addJavascriptInterface(new LocalNotificationBridge(), "AndroidLocalNotifications");
         webView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
         setContentView(webView);
+        webView.setSystemUiVisibility(0);
 
         requestNotificationPermission();
+    }
+
+    private void enableImmersiveFullscreen() {
+        Window window = getWindow();
+        if (android.os.Build.VERSION.SDK_INT >= 30) {
+            WindowInsetsController controller = window.getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) enableImmersiveFullscreen();
     }
 
     private void requestNotificationPermission() {
